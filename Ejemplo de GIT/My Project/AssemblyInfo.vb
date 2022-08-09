4acc55c6f53ad45082cc1d8913744c36f88ccba3
“VitoMoras” <morasvitoet24@gmail.com>
2022-08-08 22:22:25 -0300

    primer subida

---
+++ Ejemplo de GIT/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Ejemplo de GIT")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Ejemplo de GIT")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("64f57f05-b822-45d8-b634-cf58338be3d7")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
